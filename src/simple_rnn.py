d1f40e43c3e79550adac492b64824a7020703eeb
Matt Phillips <matt.phillips@kitware.com>
2018-03-11 12:26:26 -0400

    Added sin wave prediction demo, with poor convergence

--- src/simple_rnn.py
+++ src/simple_rnn.py
@@ -3,6 +3,7 @@ Very simple demo RNNs.
 """
 
 import argparse
+import numpy as np
 import tensorflow as tf
 
 from tensorflow.contrib.layers import fully_connected
@@ -54,9 +55,68 @@ def do_mnist():
             print("%d: Train accuracy: %f Test accuracy: %f" \
                     % (epoch, acc_train, acc_test))
 
+def predict_sin():
+    N = 300
+    t = np.linspace(0, 30, N)
+    f_t = t*np.sin(t)/3 + 2*np.sin(5*t)
+
+    n_steps = 20
+    n_inputs = 1 
+    n_neurons = 100
+    n_outputs = 1
+
+    learning_rate = 0.001
+
+    x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+    y = tf.placeholder(tf.float32, [None, n_steps, n_outputs])
+
+    cell = tf.contrib.rnn.OutputProjectionWrapper(
+            tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, 
+                activation=tf.nn.relu),
+            output_size=n_outputs)
+    outputs,states = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
+
+    loss = tf.reduce_mean( tf.square(outputs - y) )
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(loss)
+
+    init = tf.global_variables_initializer()
+
+    n_epochs = 10000
+    batch_size = 50
+
+    def _get_batch(iteration):
+        X_batch = []
+        y_batch = []
+        for i in range(batch_size):
+            idx_bgn = iteration*batch_size + i
+            idx_end = idx_bgn + n_steps
+            if idx_end == N:
+                break
+            X_batch.append( t[ idx_bgn : idx_end ] )
+            y_batch.append( f_t[ idx_bgn : idx_end ] )
+        X_batch = np.array(X_batch)
+        y_batch = np.array(y_batch)
+        X_batch = X_batch.reshape((-1, n_steps, n_inputs))
+        y_batch = y_batch.reshape((-1, n_steps, n_outputs))
+        return X_batch,y_batch 
+
+    with tf.Session() as sess:
+        init.run()
+        for epoch in range(n_epochs):
+            for iteration in range(N // batch_size):
+                X_batch,y_batch = _get_batch(iteration) 
+                sess.run(training_op, feed_dict={x: X_batch, y: y_batch})
+            if epoch % 100 == 0:
+                mse = loss.eval(session=sess, 
+                        feed_dict={x: X_batch, y: y_batch})
+                print("Epoch %d, mse: %f" % (epoch, mse))
+
+
 
 def main(args):
-    do_mnist()
+#    do_mnist()
+    predict_sin()
 
 
 if __name__=="__main__":
